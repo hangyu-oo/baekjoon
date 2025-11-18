1e9b61bdcd471fafaa61ac9628bd1bacc175c1b4
hangyu-oo <rbgurrbtjr@naver.com>
2025-11-18 16:33:56 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.c
@@ -0,0 +1,18 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+
+int main(void) {
+	int a;
+	int b;
+
+	scanf("%d", &a);
+	scanf("%d", &b);
+
+	printf("%d\n", a+b);
+	printf("%d\n", a-b);
+	printf("%d\n", a * b);
+	printf("%d\n", a / b);
+	printf("%d\n", a % b);
+}
