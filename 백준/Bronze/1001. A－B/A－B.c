a686f18c7ace691d94f65e83930cfb0821501720
hangyu-oo <rbgurrbtjr@naver.com>
2025-11-18 16:32:30 +0900

    [Bronze V] Title: A-B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.c
@@ -0,0 +1,15 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+
+int main(void) {
+	int a;
+	int b;
+	
+	scanf("%d", &a);
+	scanf("%d", &b);
+
+	printf("%d", a-b);
+}
+
