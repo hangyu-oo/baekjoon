96b04b08f18fe25130d296ff8797edff757084cb
hangyu-oo <rbgurrbtjr@naver.com>
2025-11-18 20:01:17 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.c
@@ -0,0 +1,16 @@
+#pragma warning(disable:4996) 
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int x; 
+	int y;
+	
+	y = 543;
+
+	scanf("%d", &x);
+
+	printf("%d",x-y);
+
+	return 0;
+}
